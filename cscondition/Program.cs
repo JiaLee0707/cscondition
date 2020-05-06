508c7843cc4751f2f61b58d3129cfadf839f49bf
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-05-06 14:14:49 +0900

    13-1. if 조건문

--- cscondition/Program.cs
+++ cscondition/Program.cs
@@ -10,6 +10,19 @@ namespace cscondition
     {
         static void Main(string[] args)
         {
+            Console.Write("숫자 입력 : ");
+          //var
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다!");
+            }
+
+            if(input % 2 == 1)
+            {
+                Console.WriteLine("홀수입니다!");
+            }
         }
     }
 }
